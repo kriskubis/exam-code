0e9dd391737b0f3192f5081b47d4ee0423ed25fc
Kristian Kubis <44458501+kriskubis@users.noreply.github.com>
2020-06-01 13:14:33 +0200

    Base code without exam additions

---
+++ tiamoto/js/admin.js
@@ -0,0 +1,289 @@
+//----------------------- SINGLE-PAGE FUNCTIONALITY -----------------------//
+// Change active button (the colour green) with jQuery
+$(function() {
+  $(".btn").click(function() {
+      $(".btn").removeClass("btn-active");
+      $(this).addClass("btn-active");  
+  });
+});
+
+// Hide and show sections
+$(function() {
+  $("#addBtn").click(function() {
+      $("#admin-title").text("Adding new activity");
+      $("#edit").addClass("hidden");
+      $("#imgUpload").addClass("hidden");
+      $("#add").removeClass("hidden");
+  });
+});
+
+$(function() {
+  $("#editBtn").click(function() {
+      $("#admin-title").text("Editing existing activity");
+      $("#add").addClass("hidden");
+      $("#imgUpload").addClass("hidden");
+      $("#edit").removeClass("hidden");
+  });
+});
+
+$(function() {
+  $("#imgBtn").click(function() {
+      $("#admin-title").text("Upload new image");
+      $("#edit").addClass("hidden");
+      $("#add").addClass("hidden");
+      $("#imgUpload").removeClass("hidden");
+  });
+});
+
+function addNewActivity() {
+  // Fields
+  let title = document.querySelector('#title').value;
+
+  // Fail message
+  if (title === "") {
+    alert('Oops, something is missing. Please fill in the field named "Activity Title".');
+  } else {
+
+    // Popularity ranking
+    var catScore = 0;
+    var category = document.querySelector('input[name="cat"]:checked').value;
+
+    if ((category == 'dining') || (category == 'city')) {
+      catScore = 3;
+    } else if (category == 'outdoor') {
+      catScore = 2;
+    } else if ((category == 'amusement') || (category == 'learning')) {
+      catScore = 1;
+    } else {
+      catScore = 0;
+    }
+
+    let newActivity = {
+      title: document.querySelector('#title').value,
+      aud: document.querySelector('input[name="aud"]:checked').value,
+      cat : document.querySelector('input[name="cat"]:checked').value,
+      price : document.querySelector('input[name="price"]:checked').value,
+      indoors : document.querySelector('input[name="indoors"]:checked').value,
+      friendly : document.querySelector('input[name="friendly"]:checked').value,
+      dist: document.querySelector('#dist').value,
+      intro : document.querySelector('#intro').value,
+      ideal : document.querySelector('#ideal').value,
+      street: document.querySelector('#street').value,
+      city : document.querySelector('#city').value,
+      postal : document.querySelector('#postal').value,
+      lat : document.querySelector('#lat').value,
+      lng : document.querySelector('#lng').value,
+      why : document.querySelector('#why').value,
+      cant : document.querySelector('#cant').value,
+      good : document.querySelector('#good').value,
+      monO : document.querySelector('#monO').value,
+      monC : document.querySelector('#monC').value,
+      tueO : document.querySelector('#tueO').value,
+      tueC : document.querySelector('#tueC').value,
+      wedO : document.querySelector('#wedO').value,
+      wedC : document.querySelector('#wedC').value,
+      thuO : document.querySelector('#thuO').value,
+      thuC : document.querySelector('#thuC').value,
+      friO : document.querySelector('#friO').value,
+      friC : document.querySelector('#friC').value,
+      satO : document.querySelector('#satO').value,
+      satC : document.querySelector('#satC').value,
+      sunO : document.querySelector('#sunO').value,
+      sunC : document.querySelector('#sunC').value,
+      comment : document.querySelector('#comment').value,
+      score : catScore,
+      img : document.querySelector('#img').value,
+      web : document.querySelector('#web').value
+    };
+  
+    console.log(newActivity);
+    // Create new document with the title as ID
+    actRef.doc(title).set(newActivity);
+
+    alert("Success! " + title + " was added to the guide.");
+    document.getElementById("add-form").reset(); // Reset the form
+  }
+}
+
+//----------------------- EDIT ACTIVITY -----------------------//
+actRef.onSnapshot(function(snapshotData) {
+  let acts = snapshotData.docs;
+  appendFields(acts);
+});
+
+// Edit fields based on chosen activity
+function appendFields(acts) {
+  let htmlTemplate = "";
+  for (let act of acts) {
+    htmlTemplate += `
+    <article class="option" onClick="
+    document.getElementById('id').value = '${act.data().title}';
+    document.getElementById('delete-text').innerHTML = 'Delete ${act.data().title}';
+
+    document.getElementById('new-title').value = '${act.data().title}';
+    document.getElementById('new-aud').value = '${act.data().aud}';
+    document.getElementById('new-cat').value = '${act.data().cat}';
+    document.getElementById('new-price').value = '${act.data().price}';
+    document.getElementById('new-indoors').value = '${act.data().indoors}';
+    document.getElementById('new-dist').value = '${act.data().dist}';
+    document.getElementById('new-friendly').value = '${act.data().friendly}';
+
+    document.getElementById('new-intro').value = '${act.data().intro}';
+    document.getElementById('new-ideal').value = '${act.data().ideal}';
+    document.getElementById('new-why').value = '${act.data().why}';
+    document.getElementById('new-cant').value = '${act.data().cant}';
+    document.getElementById('new-good').value = '${act.data().good}';
+
+    document.getElementById('new-street').value = '${act.data().street}';
+    document.getElementById('new-postal').value = '${act.data().postal}';
+    document.getElementById('new-city').value = '${act.data().city}';
+    document.getElementById('new-lat').value = '${act.data().lat}';
+    document.getElementById('new-lng').value = '${act.data().lng}';
+
+    document.getElementById('new-monO').value = '${act.data().monO}';
+    document.getElementById('new-tueO').value = '${act.data().tueO}';
+    document.getElementById('new-wedO').value = '${act.data().wedO}';
+    document.getElementById('new-thuO').value = '${act.data().thuO}';
+    document.getElementById('new-friO').value = '${act.data().friO}';
+    document.getElementById('new-satO').value = '${act.data().satO}';
+    document.getElementById('new-sunO').value = '${act.data().sunO}';
+    document.getElementById('new-monC').value = '${act.data().monC}';
+    document.getElementById('new-tueC').value = '${act.data().tueC}';
+    document.getElementById('new-wedC').value = '${act.data().wedC}';
+    document.getElementById('new-thuC').value = '${act.data().thuC}';
+    document.getElementById('new-friC').value = '${act.data().friC}';
+    document.getElementById('new-satC').value = '${act.data().satC}';
+    document.getElementById('new-sunC').value = '${act.data().sunC}';
+    document.getElementById('new-comment').value = '${act.data().comment}';
+
+    document.getElementById('new-img').value = '${act.data().img}';
+    document.getElementById('new-web').value = '${act.data().web}';
+    
+    ">
+    <h6>${act.data().title}</h6>
+    </article>        
+  `;
+  }
+  document.querySelector('#edit-activity').innerHTML = htmlTemplate;
+}
+
+// Update functionality
+function editActivity() {
+  // Popularity ranking
+  var catScore = 0;
+  var category = document.querySelector('#new-cat').value;
+
+  if ((category == 'dining') || (category == 'city')) {
+    catScore = 3;
+  } else if (category == 'outdoor') {
+    catScore = 2;
+  } else if ((category == 'amusement') || (category == 'learning')) {
+    catScore = 1;
+  } else {
+    catScore = 0;
+  }
+
+  // Fields
+  let title = document.querySelector('#id').value;
+  let newTitle = document.querySelector('#new-title').value;
+  let newCat = document.querySelector('#new-cat').value;
+  let newAud = document.querySelector('#new-aud').value;
+  let newPrice = document.querySelector('#new-price').value;
+  let newIndoors = document.querySelector('#new-indoors').value;
+  let newDist = document.querySelector('#new-dist').value;
+  let newFriendly = document.querySelector('#new-friendly').value;
+  let newIntro = document.querySelector('#new-intro').value;
+  let newIdeal = document.querySelector('#new-ideal').value;
+  let newWhy = document.querySelector('#new-why').value;
+  let newCant = document.querySelector('#new-cant').value;
+  let newGood = document.querySelector('#new-good').value;
+  let newMonO = document.querySelector('#new-monO').value;
+  let newMonC = document.querySelector('#new-monC').value;
+  let newTueO = document.querySelector('#new-tueO').value;
+  let newTueC = document.querySelector('#new-tueC').value;
+  let newWedO = document.querySelector('#new-wedO').value;
+  let newWedC = document.querySelector('#new-wedC').value;
+  let newThuO = document.querySelector('#new-thuO').value;
+  let newThuC = document.querySelector('#new-thuC').value;
+  let newFriO = document.querySelector('#new-friO').value;
+  let newFriC = document.querySelector('#new-friC').value;
+  let newSatO = document.querySelector('#new-satO').value;
+  let newSatC = document.querySelector('#new-satC').value;
+  let newSunO = document.querySelector('#new-sunO').value;
+  let newSunC = document.querySelector('#new-sunC').value;
+  let newComment = document.querySelector('#new-comment').value;
+  let newStreet = document.querySelector('#new-street').value;
+  let newPostal = document.querySelector('#new-postal').value;
+  let newLat = document.querySelector('#new-lat').value;
+  let newLng = document.querySelector('#new-lng').value;
+  let newCity = document.querySelector('#new-city').value;
+  let newScore = catScore;
+  let newImg = document.querySelector('#new-img').value;
+  let newWeb = document.querySelector('#new-web').value;
+
+  // Fail message
+  if (newTitle === "") {
+    alert('Please fill in the field named "New title".');
+  } else {
+    let updatedActivity = {
+      oldTitle: title,
+      title: newTitle,
+      cat: newCat,
+      aud: newAud,
+      price: newPrice,
+      indoors: newIndoors,
+      dist: newDist,
+      friendly: newFriendly,
+      intro: newIntro,
+      ideal: newIdeal,
+      why: newWhy,
+      cant: newCant,
+      good: newGood,
+      monO: newMonO,
+      monC: newMonC,
+      tueO: newTueO,
+      tueC: newTueC,
+      wedO: newWedO,
+      wedC: newWedC,
+      thuO: newThuO,
+      thuC: newThuC,
+      friO: newFriO,
+      friC: newFriC,
+      satO: newSatO,
+      satC: newSatC,
+      sunO: newSunO,
+      sunC: newSunC,
+      comment: newComment,
+      street: newStreet,
+      postal: newPostal,
+      city: newCity,
+      lat: newLat,
+      lng: newLng,
+      score: newScore,
+      img: newImg,
+      web: newWeb
+    };
+  
+    // Delete old document by ID (title)
+    // and create new document with the new title as ID
+    actRef.doc(title).delete().then(function() {
+      actRef.doc(newTitle).set(updatedActivity);
+      alert("Success! " + title + " was updated.");
+    }).catch(function(error) {
+      alert("Oops! Something went wrong.");
+      console.error("Error updating document: ", error);
+    });
+  }
+}
+
+//----------------------- DELETE ACTIVITY -----------------------//
+function deleteActivity() {
+  let id = document.querySelector('#id').value;
+
+  actRef.doc(id).delete().then(function() {
+    alert("Success! " + id + " was deleted.");
+}).catch(function(error) {
+    alert("Oops! Something went wrong.");
+    console.error("Error removing document: ", error);
+});
+}
---
+++ tiamoto/js/result.js
@@ -0,0 +1,324 @@
+//----------------------- LOCAL STORAGE -----------------------//
+var whoAre = [];
+var interests = [];
+var howFar = [];
+var budget = [];
+
+function localStorageCheck() {
+  // Search
+  whoAre = localStorage.getItem('q1');
+  interests = localStorage.getItem('q2');
+  howFar = localStorage.getItem('q3');
+  budget = localStorage.getItem('q4');
+
+  console.log('In local storage: ' + whoAre + ', ' + interests + ', ' + howFar + ', ' + budget);
+}
+
+localStorageCheck();
+
+//----------------------- MAPBOX -----------------------//
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFpZXZlbiIsImEiOiJjazlsYzZhN2QwMzV3M29xdTF5N3gzM3dwIn0.cvTTAriBgANW9W8N9auz9g';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/maieven/ck9lcgut327y51inypjxttjkr', // stylesheet location
+    center: [10.500,56.380], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+});
+
+// Expand map
+$(function() {
+  $("#expand-map").click(function() {
+      $(".map").toggleClass("expanded");
+      $(".expand").toggleClass("rotate");
+  });
+});
+
+// Refresh map or it won't fill the div on expand
+$(function(){
+  $("#expand-map").click(function() {
+  map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/maieven/ck9lcgut327y51inypjxttjkr',
+      center: [10.500,56.380],
+      zoom: 9
+    });
+  });
+});
+
+//----------------------- SEND TO MAIL -----------------------//
+// Get the modal
+var modal = document.getElementById("mailModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("mailBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//----------------------- ADD ACTIVITIES TO DOM -----------------------//
+actRef.orderBy("score","desc").onSnapshot(function(snapshotData) {
+  // Add .limit(10) after actRef.orderBy("score","desc") to limit entries.
+  let acts = snapshotData.docs;
+  appendActivities(acts);
+});
+
+// append posts to the DOM
+function appendActivities(acts) {
+  let htmlTemplate = "";
+  for (let act of acts) {
+      
+      // Converte distance from string to number (Passed as string to Firebase by default)
+      var distance = act.data().dist;
+      var distNumber = parseInt(distance, 10);
+
+      // Converte dollars to number (Passed as string to Firebase by default)
+      var budgetLength = budget.length;
+      var price = act.data().price;
+      var priceLength = price.length;
+
+      // Converte category from string to array (Passed as string to Firebase by default)
+      var category = act.data().cat;
+      var catArray = category.split();
+
+      // Use address to get direction from Google
+      var street = act.data().street;
+      var streetSep = street.split(' ').join('+'); // Swap spaces with plus sign
+      var direction = 'www.google.com/maps/dir//' + streetSep + ',' + act.data().postal + ',' + act.data().city;
+
+      //----------------------- HOURS - OPEN / CLOSED -----------------------//
+      currentDate = new Date();
+      weekday = currentDate.toString().substring(0,3).toLowerCase(); // mon, tue, wed ...
+      
+      var startTime = '00:00:00';
+      var endTime = '00:00:00';
+    
+      if (weekday == 'mon') {
+        startTime = act.data().monO + ':00';
+        endTime = act.data().monC + ':00';
+
+      } else if (weekday == 'tue') {
+        startTime = act.data().tueO + ':00';
+        endTime = act.data().tueC + ':00';
+
+      } else if (weekday == 'wed') {
+        startTime = act.data().wedO + ':00';
+        endTime = act.data().wedC + ':00';
+
+      } else if (weekday == 'thu') {
+        startTime = act.data().tueO + ':00';
+        endTime = act.data().tueC + ':00';
+
+      } else if (weekday == 'fri') {
+        startTime = act.data().friO + ':00';
+        endTime = act.data().friC + ':00';
+
+      } else if (weekday == 'sat') {
+        startTime = act.data().satO + ':00';
+        endTime = act.data().satC + ':00';
+        
+      } else if (weekday == 'sun') {
+        startTime = act.data().sunO + ':00';
+        endTime = act.data().sunC + ':00';
+
+      }
+
+      startDate = new Date(currentDate.getTime());
+      startDate.setHours(startTime.split(":")[0]);
+      startDate.setMinutes(startTime.split(":")[1]);
+      startDate.setSeconds(startTime.split(":")[2]);
+
+      endDate = new Date(currentDate.getTime());
+      endDate.setHours(endTime.split(":")[0]);
+      endDate.setMinutes(endTime.split(":")[1]);
+      endDate.setSeconds(endTime.split(":")[2]);
+
+      isOpen = startDate < currentDate && endDate > currentDate
+      
+      var openClosed = '';
+      
+      if (isOpen === true) {
+        openClosed = 'Open';
+      } else {
+        openClosed = 'Closed';
+      }
+
+    //----------------------- HTML TEMPLATES -----------------------//
+    // If statement will filter by the search saved in local storage (see in console)
+    if ((whoAre == 'challenged') && (act.data().friendly == 'yes') && (catArray.some(r=> interests.indexOf(r) >=0)) && (distNumber <= howFar) && (priceLength <= budgetLength)) {
+      console.log('"' + act.data().title + '" is applicable for this search.')
+
+      htmlTemplate += `
+      <article class="activityCard" onClick="
+        var latitude = ${act.data().lat};
+        var longitude = ${act.data().lng};
+
+        new mapboxgl.Map({
+          container: 'minimap',
+          style: 'mapbox://styles/maieven/ck9lcgut327y51inypjxttjkr',
+          center: {lat: latitude, lng: longitude},
+          zoom: 14,
+          interactive: false
+        });
+        window.scrollTo(0, 0);
+        document.getElementById('return').innerHTML = '<h4>Go back</h4>'; 
+        document.getElementById('activityGrid').classList.add('hidden');
+        document.getElementById('result-lead').classList.add('hidden');
+        document.getElementById('first-column').classList.add('first-column-act');
+        document.getElementById('return-div').style.width = '100%';
+        document.getElementById('expand-map').classList.add('hidden');
+        document.getElementById('map').classList.add('hidden');
+        document.getElementById('banner').style.backgroundImage = 'url(uploads/${act.data().img})';
+        document.getElementById('mailBtn').classList.remove('hidden');
+        document.getElementById('dist-price').classList.remove('hidden');
+        document.getElementById('sendMail').href = 'mailto:${usermail}?subject=${act.data().title}&body=${act.data().street}%0D%0A${act.data().postal}%0D%0A${act.data().city}';
+        document.getElementById('webBtn').classList.remove('hidden');
+        document.getElementById('webBtn').href = '${act.data().web}';
+        document.getElementById('info-wrapper').classList.remove('hidden');
+        document.getElementById('categories-wrapper').classList.add('hidden');
+        document.getElementById('mon').innerHTML = '${act.data().monO} - ${act.data().monC}';
+        document.getElementById('tue').innerHTML = '${act.data().tueO} - ${act.data().tueC}';
+        document.getElementById('wed').innerHTML = '${act.data().wedO} - ${act.data().wedC}';
+        document.getElementById('thu').innerHTML = '${act.data().thuO} - ${act.data().thuC}';
+        document.getElementById('fri').innerHTML = '${act.data().friO} - ${act.data().friC}';
+        document.getElementById('sat').innerHTML = '${act.data().satO} - ${act.data().satC}';
+        document.getElementById('sun').innerHTML = '${act.data().sunO} - ${act.data().sunC}';
+        document.getElementById('getDirection').href = 'https:${direction}';
+        document.getElementById('mailBtn').href = '${act.data().web}';
+        document.getElementById('webBtn').href = '${act.data().web}';
+        document.getElementById('comment').innerHTML = '${act.data().comment}';
+        document.getElementById('street').innerHTML = '${act.data().street}';
+        document.getElementById('postal').innerHTML = '${act.data().postal}';
+        document.getElementById('city').innerHTML = '${act.data().city}';
+        document.getElementById('page-title').innerHTML = '${act.data().title}';
+        document.getElementById('dist-price').innerHTML = 'Distance: ${act.data().dist} km<br>Price class: ${act.data().price}';
+        document.getElementById('page-info').innerHTML = '<div><p>${act.data().intro}</p><br><h5>Ideal for</h5><p>${act.data().ideal}</p><br><h5>Why we like it</h5><p>${act.data().why}</p><br></div><div><h5>Do not miss</h5><p>${act.data().cant}</p><br><h5>Good to know</h5><p>${act.data().good}</p></div>';" 
+        style="background-image: url('uploads/${act.data().img}');">
+        <div class="card-head">
+          <h4>${act.data().title}</h4>
+          <h5>${openClosed}<br>${act.data().dist} km</h5>
+        </div>
+        <div class="card-description">
+          <h6>${act.data().title}</h6>
+          <p>${act.data().intro}</p>
+        </div>
+        <div class="${act.data().cat}-bg card-btn">
+        <img src="media/plus.svg" alt="read more" style="width: 80%; padding: 10%; color: black;">
+        </div>
+      </article>        
+      `;
+    } else if ((act.data().aud == whoAre) && (catArray.some(r=> interests.indexOf(r) >=0)) && (distNumber <= howFar) && (priceLength <= budgetLength)) {
+      console.log('"' + act.data().title + '" is applicable for this search.')
+
+      htmlTemplate += `
+      <article class="activityCard" onClick="
+        var latitude = ${act.data().lat};
+        var longitude = ${act.data().lng};
+
+        new mapboxgl.Map({
+          container: 'minimap',
+          style: 'mapbox://styles/maieven/ck9lcgut327y51inypjxttjkr',
+          center: {lat: latitude, lng: longitude},
+          zoom: 14,
+          interactive: false
+        });
+        window.scrollTo(0, 0);
+        document.getElementById('return').innerHTML = '<h4>Go back</h4>'; 
+        document.getElementById('activityGrid').classList.add('hidden');
+        document.getElementById('result-lead').classList.add('hidden');
+        document.getElementById('first-column').classList.add('first-column-act');
+        document.getElementById('return-div').style.width = '100%';
+        document.getElementById('expand-map').classList.add('hidden');
+        document.getElementById('map').classList.add('hidden');
+        document.getElementById('banner').style.backgroundImage = 'url(uploads/${act.data().img})';
+        document.getElementById('mailBtn').classList.remove('hidden');
+        document.getElementById('dist-price').classList.remove('hidden');
+        document.getElementById('sendMail').href = 'mailto:${usermail}?subject=${act.data().title}&body=${act.data().street}%0D%0A${act.data().postal}%0D%0A${act.data().city}';
+        document.getElementById('webBtn').classList.remove('hidden');
+        document.getElementById('webBtn').href = '${act.data().web}';
+        document.getElementById('info-wrapper').classList.remove('hidden');
+        document.getElementById('categories-wrapper').classList.add('hidden');
+        document.getElementById('mon').innerHTML = '${act.data().monO} - ${act.data().monC}';
+        document.getElementById('tue').innerHTML = '${act.data().tueO} - ${act.data().tueC}';
+        document.getElementById('wed').innerHTML = '${act.data().wedO} - ${act.data().wedC}';
+        document.getElementById('thu').innerHTML = '${act.data().thuO} - ${act.data().thuC}';
+        document.getElementById('fri').innerHTML = '${act.data().friO} - ${act.data().friC}';
+        document.getElementById('sat').innerHTML = '${act.data().satO} - ${act.data().satC}';
+        document.getElementById('sun').innerHTML = '${act.data().sunO} - ${act.data().sunC}';
+        document.getElementById('getDirection').href = 'https:${direction}';
+        document.getElementById('mailBtn').href = '${act.data().web}';
+        document.getElementById('webBtn').href = '${act.data().web}';
+        document.getElementById('comment').innerHTML = '${act.data().comment}';
+        document.getElementById('street').innerHTML = '${act.data().street}';
+        document.getElementById('postal').innerHTML = '${act.data().postal}';
+        document.getElementById('city').innerHTML = '${act.data().city}';
+        document.getElementById('page-title').innerHTML = '${act.data().title}';
+        document.getElementById('dist-price').innerHTML = 'Distance: ${act.data().dist} km<br>Price class: ${act.data().price}';
+        document.getElementById('page-info').innerHTML = '<div><p>${act.data().intro}</p><br><h5>Ideal for</h5><p>${act.data().ideal}</p><br><h5>Why we like it</h5><p>${act.data().why}</p><br></div><div><h5>Do not miss</h5><p>${act.data().cant}</p><br><h5>Good to know</h5><p>${act.data().good}</p></div>';" 
+        style="background-image: url('uploads/${act.data().img}');">
+        <div class="card-head">
+          <h4>${act.data().title}</h4>
+          <h5>${openClosed}<br>${act.data().dist} km</h5>
+        </div>
+        <div class="card-description">
+          <h6>${act.data().title}</h6>
+          <p>${act.data().intro}</p>
+        </div>
+        <div class="${act.data().cat}-bg card-btn">
+        <img src="media/plus.svg" alt="read more" style="width: 80%; padding: 10%; color: black;">
+        </div>
+      </article>        
+      `;
+    }
+
+
+  }
+  document.querySelector('#activityGrid').innerHTML = htmlTemplate;
+}
+
+
+// Change site content to show activity and change button functionality
+function returnBtn() {
+  let cards = document.querySelector('#activityGrid')
+
+  if (cards.classList.contains('hidden')) {
+    document.getElementById('page-title').innerHTML = 'Recommendations';
+    document.getElementById('dist-price').innerHTML = '';
+    document.getElementById('page-info').innerHTML = '<p>We’ve listed our top picks based on your answers below.<br><br>Don’t like what you see? You can start over and choose new categories.</p>';
+    document.getElementById('dist-price').classList.add('hidden');
+    document.getElementById('return').innerHTML = '<h4>Try again</h4>'; 
+    document.getElementById('activityGrid').classList.remove('hidden');
+    document.getElementById('expand-map').classList.remove('hidden');
+    document.getElementById('map').classList.remove('hidden');
+    document.getElementById('result-lead').classList.remove('hidden');
+    document.getElementById('first-column').classList.remove('first-column-act');
+    document.getElementById('categories-wrapper').classList.remove('hidden');
+    document.getElementById('info-wrapper').classList.add('hidden');
+    document.getElementById('mailBtn').classList.add('hidden');
+    document.getElementById('webBtn').classList.add('hidden');
+    map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/maieven/ck9lcgut327y51inypjxttjkr',
+      center: [10.500,56.380],
+      zoom: 9
+    });
+  } else {
+    window.location.pathname = '/index.html';
+  }
+}
