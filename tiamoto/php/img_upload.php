50aae0545486de158d330c4e7c2a677d8d867feb
Mai Even <eaammel@students.eaaa.dk>
2020-06-08 09:55:11 +0200

    Missing images
    also deleted file size maximum on image upload

--- tiamoto/php/img_upload.php
+++ tiamoto/php/img_upload.php
@@ -23,14 +23,9 @@ if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
